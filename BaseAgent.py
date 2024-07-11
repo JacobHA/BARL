4d8f26030bf8126877d276cdef15cc398a3b1b1a
Jacob Adamczyk <42879357+JacobHA@users.noreply.github.com>
2024-07-11 11:12:06 -0400

    moved callbacks

--- BaseAgent.py
+++ BaseAgent.py
@@ -20,22 +20,6 @@ def get_new_params(base_cls_obj, locals):
     return {**locals, **base_class_kwargs}
 
 
-class EvalCallback:
-    def __init__(self, agent: 'BaseAgent'):
-        self.agent = agent
-
-
-class AUCCallback(EvalCallback):
-    def __init__(self, agent: 'BaseAgent'):
-        super().__init__(agent)
-        self.auc = 0
-    def __call__(self, state=None, action=None, reward=None, done=None, end=False):
-        if end:
-            self.agent.log_history('eval/auc', self.auc, self.agent.learn_env_steps)
-            return
-        self.auc += reward
-
-
 class BaseAgent:
     @typechecked
     def __init__(self,
